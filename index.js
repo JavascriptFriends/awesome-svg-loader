1b6ca44e32453b3fb343f0a785640c1f2a349130
Jackes Lemos <jackes.lemos@kaiostech.com>
2018-05-17 13:07:21 -0400

    Changed versions

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const babel = require("babel-core");
 const fs = require("fs");
 
 
-
 module.exports = function (content, map, meta) {
     const fileexist = this.
     const fileBuffer = this.fs.readFileSync(this.resourcePath);
     const svgResult = fileBuffer.toString();
-    const strFn = `export default function(props) { return (${svgResult}) }`;
+    const strFn = `export default function({className}) { return (${svgResult}) }`;
 
     const babelOptions = {
         plugins: [
